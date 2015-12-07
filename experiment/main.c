288e03dc32eed43c1d82b5ef7fa194634e91b0e1
Toxni <i@toxni.com>
2015-12-07 14:59:15 +0800

    Initial Commit

---
+++ experiment/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  experiment
+//
+//  Created by Toxni on 12/7/15.
+//  Copyright © 2015 Toxni. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
